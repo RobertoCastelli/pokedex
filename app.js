af4b4b3f48274c945e74fd985e46241645ab7cfd
RobertoCastelli <roberto.castelli.teal@gmail.com>
2019-12-02 00:19:42 +0100

    still working

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 /*
  * TODO
- * Sistemare ricerca dopo not found
  * Sistemare tasto Up - Down
+ * mettere easter egg
+ * metter sound
 */
 
 let screen = document.getElementById('poke-image');
@@ -24,14 +25,32 @@ let infoDEF = document.getElementById('info-def');
 
 let key = 0;
 
-async function searchPokemon() {
-    if (key < 1) window.location.reload();
+
+async function searchPokemonEvolutions() {
     try {
-    let api = 'https://pokeapi.co/api/v2/pokemon';
-    let response = await fetch(`${api}/${key}`);
-    let data = await response.json();
-    getPokemon(data);
-    key = parseInt(pokeID.innerText);
+        let apiEvo = 'https://pokeapi.co/api/v2/evolution-chain';
+        let responseEvo = await fetch(`${apiEvo}/${key}`);
+        let dataEvo = await responseEvo.json();
+
+        let evo0 = dataEvo.chain.species.name;
+        let evo1 = dataEvo.chain.evolves_to[0].species.name;
+        let evo2 = dataEvo.chain.evolves_to[0].evolves_to[0].species.name;
+     
+        displayPokemonEvo(evo0);
+        searchPokemonInfo(evo0);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+async function searchPokemonInfo(pokemonName) {
+    try {
+        let api = 'https://pokeapi.co/api/v2/pokemon';
+        let response = await fetch(`${api}/${pokemonName}`);
+        let data = await response.json();
+        
+        displayPokemonInfo(data);
     }
     catch(error) {
         pokeName.innerText = ('not found!');
@@ -40,61 +59,57 @@ async function searchPokemon() {
     }
 }
 
-btnSearch.addEventListener('click', () => {
-    if (pokeInput.value == '') { 
-    pokeName.innerText = 'Zzz... name?'
-    } else {
-    key = pokeInput.value.toLowerCase();
-    searchPokemon();
-    }
-})
+// // RELOAD APP
+// btnSwitch.addEventListener('click', () => {
+//     window.location.reload();
+// })
 
+// // SEARCH POKEMON
+// btnSearch.addEventListener('click', () => {
+//     if (pokeInput.value == '') { 
+//     pokeName.innerText = 'Zzz... name?'
+//     } else {
+//     key = pokeInput.value.toLowerCase();
+//     searchPokemon();
+//     }
+// })
+
+// SCROLL POKEMON LIST -->
 btnRight.addEventListener('click', () => {
     key += 1;
-    searchPokemon();
+    searchPokemonEvolutions();
+   
 })
 
+// SCROLL POKEMON LIST <--
 btnLeft.addEventListener('click', () => {
     key -= 1;
-    searchPokemon();
+    searchPokemonEvolutions(0);
 })
 
-btnUp.addEventListener('click', () => {
-    if (key % 3 == 0) {
-        key += 3
-        searchPokemon();
-    } else {
-        while (key % 3 != 0) {
-            key += 1;
-        }
-    }  
-    searchPokemon();
-})
+// UP 
+// btnUp.addEventListener('click', () => {
+//     searchPokemonEvolutions();
+// })
 
-btnDown.addEventListener('click', () => {
-    if (key % 3 == 0) {
-        key -= 3
-        searchPokemon();
-    } else {
-        while (key % 3 != 0) {
-            key -= 1;
-        }
-    }  
-    searchPokemon();
-})
+// // DOWN 
+// btnDown.addEventListener('click', () => {
 
+// })
 
-btnSwitch.addEventListener('click', () => {
-    window.location.reload();
-})
-
-function getPokemon(pokemon) {
-    screen.src = pokemon.sprites.front_default;
-    pokeName.innerText = `${pokemon.name}`.toUpperCase();
-    pokeID.innerText = pokemon.id < 10 ? `0${pokemon.id}`:`${pokemon.id}`
-    infoType.innerText = `type: ${pokemon.types[0].type.name}`;
-    infoHP.innerText = `health: ${pokemon.stats[5].stat.name, pokemon.stats[5].base_stat}`;
-    infoSP.innerText = `speed: ${pokemon.stats[0].stat.name, pokemon.stats[0].base_stat}`;
-    infoATK.innerText = `attack: ${pokemon.stats[3].stat.name, pokemon.stats[3].base_stat}`;
-    infoDEF.innerText = `defence: ${pokemon.stats[4].stat.name, pokemon.stats[4].base_stat}`;  
+// POPULATE POKEMON INFO + IMAGE
+function displayPokemonEvo(pokemonEvolution) {
+    pokeName.innerText = pokemonEvolution.toUpperCase();
 }
+
+function displayPokemonInfo(pokemonInfo) {
+    screen.src = pokemonInfo.sprites.front_default;
+    pokeID.innerText = pokemonInfo.id < 10 ? `0${pokemonInfo.id}`:`${pokemonInfo.id}`
+    infoType.innerText = `type: ${pokemonInfo.types[0].type.name}`;
+    infoHP.innerText = `health: ${pokemonInfo.stats[5].stat.name, pokemonInfo.stats[5].base_stat}`;
+    infoSP.innerText = `speed: ${pokemonInfo.stats[0].stat.name, pokemonInfo.stats[0].base_stat}`;
+    infoATK.innerText = `attack: ${pokemonInfo.stats[3].stat.name, pokemonInfo.stats[3].base_stat}`;
+    infoDEF.innerText = `defence: ${pokemonInfo.stats[4].stat.name, pokemonInfo.stats[4].base_stat}`;  
+}
+
+
